c5a3b753c7ad0393e2f8d36518aa8b910d471071
Souparnika-7120 <sskpjagadesh@gmail.com>
2026-05-04 11:50:08 +0530

    Update ultrasonic_buzzer.ino

--- code/ultrasonic_buzzer.ino
+++ code/ultrasonic_buzzer.ino
@@ -1,1 +1,30 @@
+#include <NewPing.h>
 
+#define TRIGGER_PIN 32
+#define ECHO_PIN 33
+#define MAX_DISTANCE 200
+#define BUZZER_PIN 2
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(BUZZER_PIN, OUTPUT);
+}
+
+void loop(){
+  unsigned int distance = sonar.ping_cm();
+
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  if(distance > 0 && distance < 20){
+    digitalWrite(BUZZER_PIN, HIGH);  // buzzer ON
+  }
+  else{
+    digitalWrite(BUZZER_PIN, LOW);   // buzzer OFF
+  }
+
+  delay(200);   // small delay for stability
+}
